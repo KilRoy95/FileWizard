0c07447020d28d4464d3a77eb295601b5caee32d
KilRoy95 <Leopwn@hotmail.com>
2023-12-01 06:58:40 +0100

    Initial commit

---
+++ src/test/java/com/example/FileWizard/FileWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FileWizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
